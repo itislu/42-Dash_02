5c0a2b1bb15627fb0237456fc37e1d1319d532b8
Bernhard Gebetsberger <bgebetsb@student.42vienna.com>
2025-04-10 11:33:43 +0200

    feat: first algo implementation

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,29 @@
 use std::{env, fs::read_to_string};
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Clone)]
 enum MapFieldType {
     Node,
     Space(usize),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct MapField {
     field_type: MapFieldType,
+    covered: bool,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Map {
     id: usize,
     grid: Vec<Vec<MapField>>,
     height: usize,
     width: usize,
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Position {
+    row: usize,
+    col: usize,
 }
 
 fn parse_map(input: &str, id: usize) -> Map {
@@ -27,9 +34,11 @@ fn parse_map(input: &str, id: usize) -> Map {
                 .map(|c| match c {
                     '*' => MapField {
                         field_type: MapFieldType::Node,
+                        covered: false,
                     },
                     value => MapField {
                         field_type: MapFieldType::Space((value as u8 - b'0').into()),
+                        covered: false,
                     },
                 })
                 .collect::<Vec<_>>()
@@ -46,6 +55,105 @@ fn parse_map(input: &str, id: usize) -> Map {
     }
 }
 
+impl Map {
+    fn uncovered_nodes(&self, pos: Position, radius: usize) -> usize {
+        let min_col = if radius > pos.col {
+            0
+        } else {
+            pos.col - radius
+        };
+        let min_row = if radius > pos.row {
+            0
+        } else {
+            pos.row - radius
+        };
+        let max_col = if pos.col + radius >= self.width {
+            self.width - 1
+        } else {
+            pos.col + radius
+        };
+        let max_row = if pos.row + radius >= self.height {
+            self.height - 1
+        } else {
+            pos.row + radius
+        };
+
+        let mut total = 0;
+
+        for row in min_row..=max_row {
+            for col in min_col..=max_col {
+                if self.grid[row][col].field_type == MapFieldType::Node
+                    && !self.grid[row][col].covered
+                {
+                    total += 1;
+                }
+            }
+        }
+
+        total
+    }
+
+    fn put_beacon(&mut self, pos: &Position, radius: usize) {
+        let min_col = if radius > pos.col {
+            0
+        } else {
+            pos.col - radius
+        };
+        let min_row = if radius > pos.row {
+            0
+        } else {
+            pos.row - radius
+        };
+        let max_col = if pos.col + radius >= self.width {
+            self.width - 1
+        } else {
+            pos.col + radius
+        };
+        let max_row = if pos.row + radius >= self.height {
+            self.height - 1
+        } else {
+            pos.row + radius
+        };
+
+        for row in min_row..=max_row {
+            for col in min_col..=max_col {
+                self.grid[row][col].covered = true;
+            }
+        }
+    }
+}
+
+fn try_map(map: &mut Map, beacons: &[usize]) -> Vec<Position> {
+    let mut positions = Vec::new();
+
+    for beacon in beacons {
+        let mut max: Vec<(usize, Position)> = Vec::new();
+        // Maybe don't start at the border
+        for row in 0..map.height {
+            for col in 0..map.width {
+                if map.grid[row][col].field_type == MapFieldType::Node || map.grid[row][col].covered
+                {
+                    continue;
+                }
+
+                let value = map.uncovered_nodes(Position { row, col }, *beacon);
+                if max.is_empty() || max[0].0 == value {
+                    max.push((value, Position { row, col }));
+                } else if value > max[0].0 {
+                    max.clear();
+                    max.push((value, Position { row, col }));
+                }
+            }
+        }
+
+        // Check the other results too
+        map.put_beacon(&max[0].1, *beacon);
+        positions.push(max[0].1);
+    }
+
+    positions
+}
+
 fn main() {
     let args: Vec<_> = env::args().collect();
 
@@ -60,6 +168,8 @@ fn main() {
             parse_map(&content, idx - 2)
         })
         .collect();
+
+    try_map(&mut maps[0].clone(), &beacons);
 
     println!("{:?}", beacons);
     println!("{:?}", maps);
