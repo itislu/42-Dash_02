65c30b2da4f918b93e7757c9ac790da2a6c2a303
Bernhard Gebetsberger <bgebetsb@student.42vienna.com>
2025-04-10 10:14:59 +0200

    feat: Input parsing and test maps

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,66 @@
+use std::{env, fs::read_to_string};
+
+#[derive(Debug)]
+enum MapFieldType {
+    Node,
+    Space(usize),
+}
+
+#[derive(Debug)]
+struct MapField {
+    field_type: MapFieldType,
+}
+
+#[derive(Debug)]
+struct Map {
+    id: usize,
+    grid: Vec<Vec<MapField>>,
+    height: usize,
+    width: usize,
+}
+
+fn parse_map(input: &str, id: usize) -> Map {
+    let grid: Vec<_> = input
+        .lines()
+        .map(|line| {
+            line.chars()
+                .map(|c| match c {
+                    '*' => MapField {
+                        field_type: MapFieldType::Node,
+                    },
+                    value => MapField {
+                        field_type: MapFieldType::Space((value as u8 - b'0').into()),
+                    },
+                })
+                .collect::<Vec<_>>()
+        })
+        .collect();
+
+    let (height, width) = (grid.len(), grid[0].len());
+
+    Map {
+        id,
+        grid,
+        height,
+        width,
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<_> = env::args().collect();
+
+    let beacons: Vec<_> = args[1]
+        .split_whitespace()
+        .map(|value| value.parse::<usize>().unwrap())
+        .collect();
+
+    let maps: Vec<_> = (2..6)
+        .map(|idx| {
+            let content = read_to_string(&args[idx]).unwrap();
+            parse_map(&content, idx - 2)
+        })
+        .collect();
+
+    println!("{:?}", beacons);
+    println!("{:?}", maps);
 }
